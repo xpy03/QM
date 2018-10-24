115c0e13e4bae5b1ae3e8c86ff26fcdccc97a28d
disen <610039018@qq.com>
2018-10-24 12:08:05 +0800

    引入Bootstrap和设计主页

--- QM/urls.py
+++ QM/urls.py
@@ -1,8 +1,17 @@
 from django.conf.urls import url, include
+from django.shortcuts import render
 
 import xadmin as admin
+
+from content.models import Category
+
+def toIndex(request):
+    # 查询一级分类
+    cates = Category.objects.filter(parent__isnull=True).all()
+    return render(request, 'index.html', locals())
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^ueditor/', include('DjangoUeditor.urls')),
+    url(r'', toIndex),
 ]
