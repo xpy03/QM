3d1f0dc444f30ee357bb20c832803be61803cb7c
disen <610039018@qq.com>
2018-10-29 13:54:07 +0800

    修复抢购bug

--- mainapps/order/views.py
+++ mainapps/order/views.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse
 from django.shortcuts import render
 
 from order.tasks import qbuy
-from utils.qbuy import get_isqbuy, query_state
+from utils.qbuy import get_isqbuy, query_state, is_buyed
 
 
 # Create your views here.
@@ -14,13 +14,19 @@ def q_read(req, book_id):
         msg['code'] = 100
         msg['msg'] = '当前用户未登录'
     else:
-        # 判断当前是否已抢完
-        if not get_isqbuy():
-            msg['code'] = 300
-            msg['msg'] = '抢购失败'
+        user_id = login_user.get('id')
+        # 判断是否已抢过
+        if is_buyed(user_id=user_id):
+            msg['code'] = 301
+            msg['msg'] = '同一用户只能抢一次'
         else:
-            # 异步执行
-            qbuy.delay(login_user.get('id'), book_id)
+            # 判断当前是否已抢完
+            if not get_isqbuy():
+                msg['code'] = 300
+                msg['msg'] = '抢购失败'
+            else:
+                # 异步执行
+                qbuy.delay(user_id, book_id)
 
     return JsonResponse(msg)
 
@@ -34,6 +40,6 @@ def qbuy_query(req, book_id):
         msg['code'] = 100
         msg['msg'] = '当前用户未登录'
 
-    user_id = login_user.get('user_id')
-    msg['code'], msg['msg'] = query_state(user_id=user_id, goods_id=book_id)
+    user_id = login_user.get('id')  # ?之前是user_id
+    msg['code'], msg['msg'] = query_state(user_id=user_id, goods_id=int(book_id))
     return JsonResponse(msg)
--- mainapps/utils/qbuy.py
+++ mainapps/utils/qbuy.py
@@ -9,11 +9,17 @@ def get_isqbuy(key='qbuy', max_size=5):
     return redis_cache.hlen(key) < max_size
 
 
+def is_buyed(key='qbuy', user_id=None):  # 判断是否已抢过了
+    return redis_cache.hexists(key, user_id)
+
+
 def add_qbuy(key='qbuy', user_id=None, goods_id=None):
     # 判断当前用户是否已抢
     if redis_cache.hexists(key, user_id):
+        print('已抢了')
         return False  # 已抢了
 
+    print('抢成功')
     redis_cache.hset(key, user_id, goods_id)
     return True  # 抢成功
 
@@ -21,7 +27,7 @@ def add_qbuy(key='qbuy', user_id=None, goods_id=None):
 def query_state(key='qbuy', user_id=None, goods_id=None):
     if redis_cache.hexists(key, user_id):
         # 再验证book_id是否与本次查询的book_id匹配
-        book_id_ = redis_cache.hget(key, user_id).decode()
+        book_id_ = int(redis_cache.hget(key, user_id).decode())  # 转成int类型
         if book_id_ != goods_id:
             return 301, '本次只限一件商品'
         else:
